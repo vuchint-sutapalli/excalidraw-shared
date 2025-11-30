e803720f56b9c80d7fcdd7730471d1b9b74fe887
vuchint sutapalli <vuchint.sutapalli@gmail.com>
2025-11-30 17:59:12 +0530

    added guest login

--- apps/excalidraw-client/src/app/config.ts
+++ apps/excalidraw-client/src/app/config.ts
@@ -1,10 +1,6 @@
 export const BACKEND_URL =
 	process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 
-export const WS_URL =
-	process.env.NEXT_PUBLIC_WS_URL ||
-	(window.location.protocol === "https:"
-		? "wss://prod.ws.slategpt.app"
-		: "ws://localhost:8080");
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
 
 // export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
