8e5fa9534ad8c3563285438666aa5c34bf813800
vuchint sutapalli <vuchint.sutapalli@gmail.com>
2025-11-30 18:25:01 +0530

    page titles

--- apps/excalidraw-client/src/app/(auth)/signup/page.tsx
+++ apps/excalidraw-client/src/app/(auth)/signup/page.tsx
@@ -1,6 +1,12 @@
 import AuthPage from "@/components/AuthPage";
 import { Suspense } from "react";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Sign Up",
+};
+
 function Signup() {
 	return (
 		<Suspense>
